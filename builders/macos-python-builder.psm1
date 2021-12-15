688f52e680230df4e3af9a8458333779a94a6984
Mikhail Koliada <mikhailkoliada@github.com>
2021-12-15 19:37:49 +0300

    try to rebootstrap brew

--- builders/macos-python-builder.psm1
+++ builders/macos-python-builder.psm1
@@ -24,7 +24,7 @@ class macOSPythonBuilder : NixPythonBuilder {
 
     [void] PrepareEnvironment() {
 	Execute-Command -Command "sudo rm -fr /usr/local/*"
-	Execute-Command  -Command "curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash -"
+	Execute-Command  -Command "sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash -"
         Execute-Command -Command "brew install zlib"
         <#
         .SYNOPSIS
